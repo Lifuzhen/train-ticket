0d275c028a5dc334c3fc9da4c0a9e98b4db17080
李福祯 <1342786307@qq.com>
2019-11-10 21:13:42 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import React, { Component, createContext } from 'react';
+import React, {Component, lazy, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
-const BatteryContext = createContext();
-const OnlineContext = createContext();
 
-class Leaf extends React.Component{
-    render(){
-        return <BatteryContext.Consumer>
-            {
-                battery => (
-                    <OnlineContext.Consumer>
-                        {
-                            online=><h1>Battery:{battery},Online:{String(online)}</h1>
-                        }
-                    </OnlineContext.Consumer>
-                )
-            }
-        </BatteryContext.Consumer>
-    }
-}
-
-class Middle extends React.Component{
-    render(){
-        return <Leaf/>
-    }
-}
+const About = lazy(() => import('./About.js'));
 
 class App extends Component {
-    state = {
-        battery:60,
-        online:false
+
+    render() {
+        return <div>
+            <Suspense fallback={<div>loading</div>}>
+                <About/>
+            </Suspense>
+        </div>
     }
-  render() {
-        const {battery, online} = this.state;
-    return (
-      <BatteryContext.Provider value={battery}>
-      <OnlineContext.Provider value={online}>
-          <button type="button" onClick={()=>this.setState({battery: battery-1})}>press</button>
-          <button type="button" onClick={()=>this.setState({online: !online})}>switch</button>
-          <Middle/>
-      </OnlineContext.Provider>
-      </BatteryContext.Provider>
-    );
-  }
 }
 
 export default App;
