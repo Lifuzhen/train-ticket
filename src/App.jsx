fb867db8ea7f2fb5606f0053fabf3afbdc2e626d
李福祯 <1342786307@qq.com>
2019-11-22 22:07:21 +0800

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React, {useState, useMemo, memo, useCallback} from 'react';
+import React, {useState, PureComponent, useMemo,useEffect, memo, useCallback, useRef} from 'react';
 import './App.css';
 
-const Counter = memo(function Counter(props) {
-    console.log("Counter render");
-    return <h1 onClick={props.onClick}>{props.count}</h1>
-})
+class Counter extends PureComponent{
+    speak(){
+        console.log(`new counter is: ${this.props.count}`)
+    }
+    render() {
+        const {props} =this;
+        return <h1 onClick={props.onClick}>{props.count}</h1>
+    }
+}
 
 function App(props) {
     const [count, setCount] = useState( 0);
     const [clickCount, setClickCount] = useState( 0);
+    const counterRef = useRef();
+    const it = useRef();
 
     const double = useMemo(()=>{
         return count * 2;
@@ -17,9 +24,22 @@ function App(props) {
     const onClick = useCallback(() =>{
         console.log("Click");
         setClickCount((clickCount)=>clickCount+1);
-    },[]);
+        counterRef.current.speak();
+    },[counterRef]);
 
     //如果useMemo返回的是一个函数就等价于useCallback
+
+    useEffect(()=>{
+        it.current = setInterval(()=>{
+            setCount(count=>count+1)
+        },1000)
+    },[]);
+
+    useEffect(()=>{
+        if(count >= 10){
+            clearInterval(it.current);
+        }
+    });
 
     return <div>
         <button
@@ -30,7 +50,7 @@ function App(props) {
         >
             Click({count}), double({double})
         </button>
-            <Counter count={double} onClick={onClick}/>
+            <Counter ref={counterRef} count={double} onClick={onClick}/>
     </div>;
 }
 
